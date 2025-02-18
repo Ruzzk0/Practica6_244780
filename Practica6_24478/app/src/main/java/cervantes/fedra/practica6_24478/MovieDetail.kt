6a9c3fd814c28206e040f0b8a01ccd4851650b28
Ruzzk0 <146685007+Ruzzk0@users.noreply.github.com>
2025-02-18 00:37:52 -0700

    boton
    comprar ticket

--- Practica6_24478/app/src/main/java/cervantes/fedra/practica6_24478/MovieDetail.kt
+++ Practica6_24478/app/src/main/java/cervantes/fedra/practica6_24478/MovieDetail.kt
@@ -1,12 +1,12 @@
 package cervantes.fedra.practica6_24478
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +14,22 @@ class MovieDetail : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_movie_detail)
 
-        var header: ImageView = findViewById(R.id.movie_header)
-        var title: TextView = findViewById(R.id.movie_title_detail)
-        var desc: TextView = findViewById(R.id.movie_desc)
+        val header: ImageView = findViewById(R.id.movie_header)
+        val title: TextView = findViewById(R.id.movie_title_detail)
+        val desc: TextView = findViewById(R.id.movie_desc)
+        val btnBuyTicket: Button = findViewById(R.id.btn_buy_ticket)
 
         val content = intent.extras
+        if (content != null) {
+            header.setImageResource(content.getInt("header"))
+            title.text = content.getString("titulo")
+            desc.text = content.getString("sinopsis")
+        }
 
-        header.setImageResource(content!!.getInt("header"))
-        title.setText(content.getString("titulo"))
-        desc.setText(content.getString("sinopsis"))
+        // Evento para abrir SeatSelectionActivity cuando se presiona el botón
+        btnBuyTicket.setOnClickListener {
+            val intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
+        }
     }
 }
